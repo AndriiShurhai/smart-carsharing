e06fcf24a9b468ee7203a4f18675ad693685490b
eqqr12 <rostikskorik9@gmail.com>
2025-11-19 23:28:39 +0200

    Create LoginViewModel.cs

---
+++ Smart-car-sharing/Smart-car-sharing/LoginViewModel.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace SmartCarSharingApp.UI.ViewModels
+{
+    public class LoginViewModel : INotifyPropertyChanged
+    {
+        private string _email;
+        private string _password;
+
+        public string Email
+        {
+            get => _email;
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Password
+        {
+            get => _password;
+            set
+            {
+                _password = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Команда для кнопки "Увійти"
+        public ICommand LoginCommand { get; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new RelayCommand(ExecuteLogin);
+        }
+
+        private void ExecuteLogin(object obj)
+        {
+            // Тут буде логіка авторизації
+            System.Windows.MessageBox.Show($"Спроба входу:\nEmail: {Email}\nPassword: {Password}");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    // Проста реалізація RelayCommand
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> _execute;
+        private readonly Predicate<object> _canExecute;
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+    }
+}
