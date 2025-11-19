c0b04dc6ab71b6e3452d34df826072ed11bd80cf
eqqr12 <rostikskorik9@gmail.com>
2025-11-19 23:58:49 +0200

    Create LoginView.xaml.cs

---
+++ Smart-car-sharing/Smart-car-sharing/Views/LoginView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using SmartCarSharingApp.UI.ViewModels;
+
+namespace SmartCarSharingApp.UI.Views
+{
+    public partial class LoginView : UserControl
+    {
+        public LoginView()
+        {
+            InitializeComponent();
+        }
+
+        // Передача пароля у ViewModel
+        private void PasswordBox_OnPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.DataContext is LoginViewModel vm)
+            {
+                vm.Password = PasswordBox.Password;
+            }
+        }
+
+        private void NavigateToRegister_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Перехід до реєстрації.", "Навігація", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
