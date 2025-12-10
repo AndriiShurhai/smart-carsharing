c58bb9ae29e6097cb22079f93550db3e04c68431
Andrii Shurhai <shurgaiandrew335@gmail.com>
2025-12-10 20:57:43 +0200

    feat(serilog): configure serilog to write to logs/app.log (#30)
    Related: SC-30

--- Smart-car-sharing/Smart-car-sharing/App.xaml.cs
+++ Smart-car-sharing/Smart-car-sharing/App.xaml.cs
@@ -3,29 +3,48 @@ using SmartCarSharing.Data;
 using SmartCarSharing.Data.Services;
 using SmartCarSharing.Core.Services;
 using SmartCarSharingApp.UI.ViewModels;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
 
 namespace SmartCarSharingApp.UI
 {
     public partial class App : Application
     {
+        // We keep a reference to the factory so we can pass loggers to other services
+        public static ILoggerFactory? LoggerFactory { get; private set; }
+
         private AppDbContext _context;
 
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            // 1. Setup Database
+            // 1. Configure Serilog
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.File("logs/app.log", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            // 2. Create the wrapper that allows us to inject ILogger<T>
+            LoggerFactory = new SerilogLoggerFactory();
+
+            var appLoger = LoggerFactory.CreateLogger<App>();
+            appLoger.LogInformation("------------------");
+            appLoger.LogInformation("Smart Carsharing app Started");
+            appLoger.LogInformation("------------------");
+
+
+            // 3. Setup Database
             _context = new AppDbContext();
             // Ensure DB is created
             _context.Database.EnsureCreated();
 
-            // 2. Setup Services
+            // 4. Setup Services
             IAuthenticationService authService = new AuthenticationService(_context);
 
-            // 3. Setup Main ViewModel
             var mainViewModel = new MainViewModel(authService);
 
-            // 4. Show Window
             var mainWindow = new MainWindow
             {
                 DataContext = mainViewModel
@@ -37,6 +56,7 @@ namespace SmartCarSharingApp.UI
 
         protected override void OnExit(ExitEventArgs e)
         {
+            Log.CloseAndFlush();
             _context.Dispose();
             base.OnExit(e);
         }
