329b915705b707bbb2830f5793562510f935f1d4
Andrii Shurhai <shurgaiandrew335@gmail.com>
2025-12-11 12:16:35 +0200

    feat(builder-pattern): implement the Builder Pattern to generate test data (#31)
    - created CarBuilder class in Core
    - updated App.xaml.cs to seed 20 random cars if database is empty
    
    Related: SC-31

--- Smart-car-sharing/Smart-car-sharing/App.xaml.cs
+++ Smart-car-sharing/Smart-car-sharing/App.xaml.cs
@@ -6,6 +6,8 @@ using SmartCarSharingApp.UI.ViewModels;
 using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Extensions.Logging;
+using SmartCarSharing.Core.Builders;
+using SmartCarSharing.Core;
 
 namespace SmartCarSharingApp.UI
 {
@@ -39,6 +41,46 @@ namespace SmartCarSharingApp.UI
             _context = new AppDbContext();
             // Ensure DB is created
             _context.Database.EnsureCreated();
+
+            if (!_context.Cars.Any())
+            {
+                var appLogger = LoggerFactory.CreateLogger<App>();
+                appLogger.LogInformation("Database is empty. Seeding with 20 cars...");
+
+                var builder = new CarBuilder();
+                var carsToSeed = new List<Car>();
+
+                var locations =  new[] { "Smila Center", "Smila Airport", "Smila Port", "Smila Railway" };
+
+                var models = new[]
+                {
+                    ("Tesla", "Model 3", 45m),
+                    ("Toyota", "Camry", 25m),
+                    ("BMW", "X5", 60m),
+                    ("Ford", "Focus", 20m),
+                    ("Audi", "A6", 55m)
+                };
+
+                var random = new Random();
+
+                for (int i = 0; i < 20; i++)
+                {
+                    var modelInfo = models[random.Next(models.Length)];
+                    var location = locations[random.Next(locations.Length)];
+
+                    var car = builder
+                        .WithModel(modelInfo.Item1, modelInfo.Item2)
+                        .WithYear(random.Next(2018, 2025))
+                        .WithPrice(modelInfo.Item3 + random.Next(-5, 5))
+                        .WithLocation(location)
+                        .Build();
+                    carsToSeed.Add(car);
+                }
+                _context.AddRange(carsToSeed);
+                _context.SaveChanges();
+
+                appLogger.LogInformation("Successfully seeded 20 cars");
+            }
 
             // 4. Setup Services
             IAuthenticationService authService = new AuthenticationService(_context);
