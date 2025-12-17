91a9e0cef3501a6c578f6614c51e164faad0cd0a
VitaliiMarian <lllllppppp1808@gmail.com>
2025-12-17 18:51:31 +0200

    Create IBookingService.cs
    feat(core): define IBookingService interface contract

---
+++ Smart-car-sharing/SmartCarSharing.Core/Services/IBookingService.cs
@@ -0,0 +1,10 @@
+﻿using SmartCarSharing.Core;
+using System.Threading.Tasks;
+
+namespace SmartCarSharing.Core.Services
+{
+    public interface IBookingService
+    {
+        Task CreateBookingAsync(Booking booking);
+    }
+}
