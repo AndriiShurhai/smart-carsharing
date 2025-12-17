4498242b08198aade0b95e249a34fea83e7a4e66
VitaliiMarian <lllllppppp1808@gmail.com>
2025-12-17 18:51:20 +0200

    Create BookingService.cs
    feat(data): implement BookingService to save reservations to the database

---
+++ Smart-car-sharing/SmartCarSharing.Data/Services/BookingService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using SmartCarSharing.Core;
+using SmartCarSharing.Core.Services;
+using SmartCarSharing.Data;
+
+namespace SmartCarSharing.Data.Services
+{
+    public class BookingService : IBookingService
+    {
+        private readonly AppDbContext _context;
+
+        public BookingService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateBookingAsync(Booking booking)
+        {
+            // Додаємо бронювання в БД
+            _context.Bookings.Add(booking);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
