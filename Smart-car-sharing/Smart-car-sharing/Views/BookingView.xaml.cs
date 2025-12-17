b67e11fdf80fd4f738cc4f8a53b4731e59d7b071
VitaliiMarian <lllllppppp1808@gmail.com>
2025-12-17 13:51:01 +0200

    Create BookingView.xaml.cs
    feat(ui): create BookingView layout with DatePickers

---
+++ Smart-car-sharing/Smart-car-sharing/Views/BookingView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace SmartCarSharingApp.UI.Views
+{
+    public partial class BookingView : UserControl
+    {
+        public BookingView()
+        {
+            InitializeComponent();
+        }
+    }
+}
